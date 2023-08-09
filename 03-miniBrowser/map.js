618b2a062608e0a5791528d92eda939ff2d59acd
Fabio Balsan <fabiobalsan@gmail.com>
2023-08-08 22:53:31 -0300

    first commit

---
+++ 03-miniBrowser/map.js
@@ -0,0 +1,15 @@
+//TDD - Teste Driven Development
+var novoMapa = new Map();
+
+class HistoryMap{
+    
+}
+
+//Acessou um site 11/11/2011 as 11 da noite
+novoMapa.set('11/11/2011 - 23:30 - URL', {title :'Titulo do site', url :'Url Sozinha'});
+novoMapa.set('11/11/2011 - 23:30 - URL', {});
+novoMapa.set('11/11/2011 - 23:31 - URL', {});
+
+
+var resultado = novoMapa;
+console.log(resultado);
---
+++ 03-miniBrowser/set.js
@@ -0,0 +1,10 @@
+//TDD - Teste Driven Development
+var novoSet = new Set();
+
+novoSet.add('backend');
+novoSet.add('frontend');
+novoSet.add('CSS');
+novoSet.add('CSS');
+
+var resultado = novoSet;
+console.log(resultado);
